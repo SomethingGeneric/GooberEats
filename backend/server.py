b3a6e800b37ea36f5d394781f05f199f65b980e4
matt c <matt@xhec.dev>
2024-09-16 22:21:23 +0100

    add userid strings to backend (closes #3)

--- backend/server.py
+++ backend/server.py
@@ -1,31 +1,38 @@
 from flask import Flask, request
 
+import os
+
 app = Flask(__name__)
 
-def get_glob_kcount():
+if not os.path.exists('cal_data'):
+    os.makedirs('cal_data')
+
+def get_glob_kcount(id):
     try:
-        with open('calories.txt', 'r') as file:
+        with open(f'cal_data/{id}.txt', 'r') as file:
             return int(file.read())
     except FileNotFoundError:
-        with open('calories.txt', 'w') as file:
+        with open(f'cal_data/{id}.txt', 'w') as file:
             file.write('0')
         return 0
 
-def set_glob_kcount(value):
-    with open('calories.txt', 'w') as file:
+def set_glob_kcount(id, value):
+    with open(f'cal_data/{id}.txt', 'w') as file:
         file.write(str(value))
 
 @app.route('/kcount', methods=['GET', 'POST'])
 def deal():
     if request.method == 'GET':
-        return str(get_glob_kcount())
+        id = request.get_json()['id']
+        return str(get_glob_kcount(id))
     elif request.method == 'POST':
         data = request.get_json()
-        if 'kcal' in data and isinstance(data['kcal'], int):
+        if 'id' in data and 'kcal' in data and isinstance(data['kcal'], int):
+            id = data['id']
             increment = data['kcal']
-            glob_kcount = get_glob_kcount()
+            glob_kcount = get_glob_kcount(id)
             glob_kcount += increment
-            set_glob_kcount(glob_kcount)
+            set_glob_kcount(id, glob_kcount)
             return str(glob_kcount)
         else:
             return "Invalid request body!"
--- backend/test.py
+++ backend/test.py
@@ -2,7 +2,7 @@ import requests
 
 # Make a POST request with JSON data
 url = "http://127.0.0.1:5000/kcount"
-data = {"kcal": 10}
+data = {"id": "boingboingtitties", "kcal": 10}
 response = requests.post(url, json=data)
 
 # Check the response status code
@@ -10,7 +10,7 @@ if response.status_code == 200:
     print("POST request successful")
 
 # Make a GET request to verify the returned value
-response = requests.get(url)
+response = requests.get(url, json={"id": "boingboingtitties"})
 
 # Check the response status code and value
 if response.status_code == 200:
